96478b36f829b87d1e600dc66b8b1e2498dc1161
Marco Bakera <marco@bakera.de>
2017-08-16 16:20:01 +0200

    more verbose documentation.

--- config_sample.py
+++ config_sample.py
@@ -1,21 +1,25 @@
 # Copy this file to config.py and update the values below.
 #
 
+# Network settings to connect the device to.
 SSID = "My Network"
 PASS = "My Password"
 
-# configure the servo
+# configure the servo motor
 SERVOPIN = 12  # GPIO12 (D6)
 FREQUENCY = 50  # Hz
 
-# how many ms to sleep in deep sleep mode
+# how many ms to sleep in deep sleep mode after that amount of time,
+# the ESP8266 awakes again. For this to work, pin GPIO16 hat to be
+# connected with the RESET Pin.
 DEEPSLEEP_TIME = 20000
 
-# common duty cyles
-# somewhere between 0 and 1023
+# common duty cyles that must range from 0 to 1023 and should conform
+# to positions of the needle.
 LEFT = 25
 RIGHT = 125
 
-# Location for data value that should be retrieved
+# Location for data value that should be retrieved. It has to contain
+# only one interger value ranging from 0 to 100.
 DATA_URL = "http://www.example.com/data.txt"
 
