0901b8df97fb791e62987e1f1a09e395494fc98a
Marco Bakera <marco@bakera.de>
2017-08-16 16:20:50 +0200

    changed order.

--- config_sample.py
+++ config_sample.py
@@ -9,6 +9,10 @@ PASS = "My Password"
 SERVOPIN = 12  # GPIO12 (D6)
 FREQUENCY = 50  # Hz
 
+# Location for data value that should be retrieved. It has to contain
+# only one interger value ranging from 0 to 100.
+DATA_URL = "http://www.example.com/data.txt"
+
 # how many ms to sleep in deep sleep mode after that amount of time,
 # the ESP8266 awakes again. For this to work, pin GPIO16 hat to be
 # connected with the RESET Pin.
@@ -18,8 +22,4 @@ DEEPSLEEP_TIME = 20000
 # to positions of the needle.
 LEFT = 25
 RIGHT = 125
-
-# Location for data value that should be retrieved. It has to contain
-# only one interger value ranging from 0 to 100.
-DATA_URL = "http://www.example.com/data.txt"
 
