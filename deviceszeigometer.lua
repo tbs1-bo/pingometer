589825ce90bfae456f722b50202322d337a2167a
Marco Bakera <marco@bakera.de>
2017-08-21 16:43:29 +0200

    added timer, information about signal strength in topic.

--- deviceszeigometer.lua
+++ deviceszeigometer.lua
@@ -19,17 +19,22 @@ function probe_received_cb(T)
    -- print("\n\tAP - PROBE REQUEST RECEIVED".."\n\tMAC: ".. T.MAC.."\n\tRSSI: "..T.RSSI)
 
    -- check if client already seen
-   if clients[T.MAC] then
-      clients[T.MAC] = clients[T.MAC] + 1
-   else
-      clients[T.MAC] = 1
+   if clients[T.MAC] == nil then
+      clients[T.MAC] = {}
+      clients[T.MAC].count = 0
    end
 
+   clients[T.MAC].count = clients[T.MAC].count + 1
+   clients[T.MAC].rssi = T.RSSI
+
    count = 0
-   for k,v in pairs(clients) do
+   for mac,vars in pairs(clients) do
       count = count + 1
-      print("key:"..k.." val:"..v)
-      mqtt_client:publish(conf.mqtt.topic..'/probe/'..k, v, 1, 1)
+      print("mac:"..mac.." count:"..vars.count.." rssi:"..vars.rssi)
+      mqtt_client:publish(conf.mqtt.topic..'/probe/'..mac.."/count",
+			  vars.count, 1, 1)
+      mqtt_client:publish(conf.mqtt.topic..'/probe/'..mac.."/rssi",
+			  vars.rssi, 1, 1)
    end
    print("#probes: "..count)
    --                                          qos retain
@@ -50,11 +55,21 @@ function mqtt_connected_cb()
 			  probe_received_cb)
 end
 
+function time_elapsed_cb()
+   print("time over")
+   -- TODO enter deep sleep mode
+   -- https://nodemcu.readthedocs.io/en/master/en/modules/node/#nodedsleep
+end
+
 -- configure station and start connection
 sta_config={}
 sta_config.ssid = WIFI_SSID
 sta_config.pwd = WIFI_PASSWORD
 sta_config.got_ip_cb = got_ip_cb
+
+local timer = tmr.create()
+timer:register(10000, tmr.ALARM_SINGLE, time_elapsed_cb)
+timer:start()
 
 wifi.sta.config(sta_config)
 
